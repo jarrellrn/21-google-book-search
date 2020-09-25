162965c65e38cdc6e72a214923f8d5e847f75d23
jarrellrn <jarrellrn@gmail.com>
2020-09-25 18:50:06 -0500

    some changes to functional component

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/shared/Navbar/Navbar";
+import Header from "./components/shared/Header/Header";
+
+import "./App.css";
+import SavedPage from "./components/SavedPage/SavedPage";
+import HomePage from "./components/HomePage/HomePage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+        <Navbar />
+        <Header />
+        <Route path="/" exact>
+          <HomePage />
+        </Route>
+        <Route path="/saved" exact>
+          <SavedPage />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
