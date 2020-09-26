cb85d5e2ca4ceede0f7fb4325d7b323a92ca37b3
jarrellrn <jarrellrn@gmail.com>
2020-09-26 08:14:53 -0500

    adding build files

---
+++ client/build/precache-manifest.abcbc665b987f2939c7992a98e382a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d6464eab0767cacbd3e7ec89975f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2a2038107616f906623",
+    "url": "/static/css/main.e1c5312d.chunk.css"
+  },
+  {
+    "revision": "5db4596ba88c18cc82db",
+    "url": "/static/js/2.f0f92d2d.chunk.js"
+  },
+  {
+    "revision": "0e3d1f32e6a7a27a748a3d56dde7ac82",
+    "url": "/static/js/2.f0f92d2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2a2038107616f906623",
+    "url": "/static/js/main.165684c9.chunk.js"
+  },
+  {
+    "revision": "4a42fdcbdfd3cf55fea3",
+    "url": "/static/js/runtime-main.1c47a842.js"
+  }
+]);
