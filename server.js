536fc7a4eb8e625f4a9252690b4d2711d8ae3cc0
jarrellrn <jarrellrn@gmail.com>
2020-09-29 18:00:05 -0500

    more cleanup

--- server.js
+++ server.js
@@ -7,16 +7,9 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
-
-// Connect to the Mongo DB
 const url = process.env.ATLAS_URL;
 mongoose.connect(url, {
   useUnifiedTopology: true,
@@ -24,20 +17,11 @@ mongoose.connect(url, {
   useCreateIndex: true,
 });
 
-// mongoose.connect(
-//   "mongodb://localhost:27017/books",
-//   { useNewUrlParser: true, useUnifiedTopology: true },
-//   () => {
-//     console.log("successfully connected to database");
-//   }
-// );
-
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connected succesfully");
 });
 
-// Use apiRoutes
 const savedBookRouter = require("./routes/savedBookRoutes");
 
 app.use("/saved", savedBookRouter);
